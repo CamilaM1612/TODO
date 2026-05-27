46eab7a033cb0c867e424d520526af0cf5d45993
Camila Muñoz <camilamo576@gmail.com>
2026-05-27 10:37:25 -0400

    primera configuracion de oauth, hasta el momento si funciona

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,56 @@ const cors = require("cors");
 const db = require("./db");
 const app = express();
 const multer = require("multer");
+//oauth
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const session = require("express-session");
 
 app.use(cors());
 app.use(express.json());
+
+// oauth
+app.use(session({
+  secret: "secreto",
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: "913973714701-nq2objp61gcsfms5u75m03thn9mnfcj3.apps.googleusercontent.com",
+  clientID: process.env.GOOGLE_CLIENT_ID,
+clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+},
+(accessToken, refreshToken, profile, done) => {
+  return done (null, profile);
+}
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null,user);
+});
+
+app.get("/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", 
+  passport.authenticate("google", {
+    failureRedirect: "/login"
+  }),
+  (req,res) => {
+    res.send("login exitoso con google");
+  }
+);
 
 // archivo
 const storage = multer.diskStorage({
