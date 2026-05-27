a5e7c28a6eb877ee82f678f2e77380ca442221ea
Camila Muñoz <camilamo576@gmail.com>
2026-05-26 23:47:41 -0400

    se agrego pequeños cambios en backend

--- backend/server.js
+++ backend/server.js
@@ -1,125 +1,132 @@
-const express = require("express")
-const cors = require("cors")
-const db = require("./db")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const db = require("./db");
+const app = express();
 const multer = require("multer");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // archivo
 const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, "Archivos/");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-    }
+  destination: (req, file, cb) => {
+    cb(null, "Archivos/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-const subir = multer ({ storage });
-
+const subir = multer({ storage });
 
 // mostrar tareas
-app.get("/tareas",(req, res) => {
-    db.query(
-        "SELECT* FROM tareas", (err, results) =>{
-            if(err){
-                res.status(500).send({
-                    error: 'Error al obtener las tareas'
-                })
-            }else{
-                res.json(results)
-            }
-        })
-})
+app.get("/tareas", (req, res) => {
+  db.query("SELECT* FROM tareas", (err, results) => {
+    if (err) {
+      res.status(500).send({
+        error: err.message
+      });
+    } else {
+      res.json(results);
+    }
+  });
+});
 
 // agregar tarea
 app.post("/tareas", (req, res) => {
-    const {descripcion} = req.body
+  const { descripcion } = req.body;
 
-    const query = 'INSERT INTO tareas (descripcion) VALUES (?)'
+  const query = "INSERT INTO tareas (descripcion) VALUES (?)";
 
-    db.query(query, [descripcion], (err,results)=> {
-        if (err){
-            res.status(500).send({
-                error: 'Error al crear la tarea'
-            })
-        }else{
-            res.status (201).json({
-                id: results.insertId,
-                descripcion
-            })
-            
-        }
-    })
-})
+  db.query(query, [descripcion], (err, results) => {
+    if (err) {
+      res.status(500).send({
+        error:err.message
+      });
+    } else {
+      res.status(201).json({
+        success: true,
+        mensaje: "Tarea Creada",
+        data: {
+          id: results.insertId,
+          descripcion,
+        },
+      });
+    }
+  });
+});
 
 //agregar archivo
-app.post("/archivos", subir.single("archivo"), (req,res)=>{
-    res.json({
-        mensaje: "Archivo Subido",
-        archivo: req.file
-    });
+app.post("/archivos", subir.single("archivo"), (req, res) => {
+  res.json({
+    mensaje: "Archivo Subido",
+    archivo: req.file,
+  });
 });
 
 app.delete("/tareas/:id", (req, res) => {
-    const { id } = req.params
+  const { id } = req.params;
 
-    const query = 'DELETE FROM tareas WHERE id = ?'
+  const query = "DELETE FROM tareas WHERE id = ?";
 
-    db.query(query, [id], (err,results)=> {
-        if (err){
-            res.status(500).send({
-                error: 'Error al eliminar la tarea'
-            })
-        }else{
-            res.status (204).send()
-            
-        }
-    })
-})
+  db.query(query, [id], (err, results) => {
+    if (err) {
+      res.status(500).send({
+        error: err.message
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        mensaje: "Tarea eliminada"
+      });
+    }
+  });
+});
 
 app.put("/tareas/:id", (req, res) => {
-    const {id} = req.params
-    const {descripcion} = req.body
+  const { id } = req.params;
+  const { descripcion } = req.body;
 
-    const query = 'UPDATE tareas SET descripcion = ? WHERE id = ?'
-    db.query(query, [descripcion,id], (err, results) => {
-        if (err){
-            res.status(500).send({
-                error: 'Error al actualizar'
-            })
-        }else{
-            res.json({
-                id,
-                descripcion
-            })
+  const query = "UPDATE tareas SET descripcion = ? WHERE id = ?";
+  db.query(query, [descripcion, id], (err, results) => {
+    if (err) {
+      res.status(500).send({
+        error: err.message
+      });
+    } else {
+      res.json({
+        success: true,
+        mensaje: "Tarea eliminada",
+        data:{
+            id,
+            descripcion
         }
-    })
-})
-
+    
+      });
+    }
+  });
+});
 
 app.put("/tareas/estado/:id", (req, res) => {
-    const {id} = req.params
-    const {estado} = req.body
+  const { id } = req.params;
+  const { estado } = req.body;
 
-    const query = 'UPDATE tareas SET estado = ? WHERE id = ?'
-    db.query(query, [estado,id], (err, results) => {
-        if (err){
-            res.status(500).send({
-                error: 'Error al actualizar estado'
-            })
-        }else{
-            res.json({
-                id,
-                estado
-            })
-        }
-    })
-})
+  const query = "UPDATE tareas SET estado = ? WHERE id = ?";
+  db.query(query, [estado, id], (err, results) => {
+    if (err) {
+      res.status(500).send({
+        error: err.message
+      });
+    } else {
+      res.json({
+        id,
+        estado,
+      });
+    }
+  });
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("servidor funcionando")
-})
+  console.log("servidor funcionando");
+});
