18dd2344b506e99f65d3f6128720227903181647
Camila Muñoz <camilamo576@gmail.com>
2026-06-06 21:42:41 -0400

    se arreglo el readme, se agrego un ejemplo de env, se agrego la base de datos

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # To Do List
-
+Aplicaci0n web desarrollada con React, Node.js, Express y MySQL para la gestion de tareas.
 ## Requisitos
 * Node.js
 * MySQL
 * Git
 
 ## Clonar el proyecto
-git clone 
+git clone https://github.com/CamilaM1612/TODO.git
+cd TODO
 
 ## Configuración de la base de datos
 Crear una base de datos llamada:
 CREATE DATABASE todo_db;
-Importar el archivo: todo_db.sql
+Importar el archivo incluido en el proyecto:: todo_db.sql
+Desde MySQL Workbench:
+1. Abrir MySQL Workbench.
+2. Ir a Server -> Data Import.
+3 Seleccionar Import from Self-Contained File.
+4. Elegir el archivo `todo_db.sql`.
+5. Seleccionar la base de datos `todo_db`.
+6. Presionar **Start Import**.
 
 ## Configurar variables de entorno
 Crear un archivo `.env` dentro de la carpeta backend:
@@ -40,3 +48,14 @@ npm run dev
 ## Acceso
 Frontend: https://localhost:5173
 Backend: https://localhost:3000
+
+## Funcionalidades
+- Crear tareas
+- Editar tareas
+- Eliminar tareas
+- Cambiar estado de tareas
+ - Subida de archivos
+- Descarga de archivos
+- Inicio de sesión con Google OAuth
+- Cierre de sesión
+- Uso de HTTPS mediante certificados .pem
---
+++ todo_db.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: localhost    Database: todo_db
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tareas`
+--
+
+DROP TABLE IF EXISTS `tareas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tareas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) DEFAULT NULL,
+  `estado` tinyint(1) DEFAULT '0',
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `archivo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tareas`
+--
+
+LOCK TABLES `tareas` WRITE;
+/*!40000 ALTER TABLE `tareas` DISABLE KEYS */;
+INSERT INTO `tareas` VALUES (27,'Comprar pan y queso de la plaza 14 de septiembre',1,'2026-06-07 01:20:25',''),(28,'Hacer la documentacion de web',0,'2026-06-07 01:20:37',''),(29,'Subir proyecto a GitHub',0,'2026-06-07 01:20:46',''),(30,'Arreglar el README',0,'2026-06-07 01:21:02','');
+/*!40000 ALTER TABLE `tareas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-06-06 21:21:49
